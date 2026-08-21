49e0577bd249eee6c8bc1d96d2bdf750611de422
gunflo1011-debug <gunflo1011@gmail.com>
2026-08-21 07:06:29 +0200

    Support GitHub Pages subpaths in crawl assets

--- src/site-assets.js
+++ src/site-assets.js
@@ -7,8 +7,20 @@ function xmlEscape(value) {
     .replaceAll("'", '&apos;');
 }
 
+function normalizedBase(baseUrl) {
+  const url = new URL(baseUrl);
+  url.pathname = `${url.pathname.replace(/\/$/, '')}/`;
+  url.search = '';
+  url.hash = '';
+  return url;
+}
+
+function publicUrl(baseUrl, path) {
+  const base = normalizedBase(baseUrl);
+  return new URL(String(path).replace(/^\//, ''), base).toString();
+}
+
 export function renderRobotsTxt({ baseUrl }) {
-  const origin = new URL(baseUrl).origin;
   return [
     'User-agent: *',
     'Allow: /',
@@ -17,15 +29,14 @@ export function renderRobotsTxt({ baseUrl }) {
     'User-agent: OAI-SearchBot',
     'Allow: /',
     '',
-    `Sitemap: ${origin}/sitemap.xml`,
+    `Sitemap: ${publicUrl(baseUrl, 'sitemap.xml')}`,
     ''
   ].join('\n');
 }
 
 export function renderSitemap({ baseUrl, pages }) {
-  const origin = new URL(baseUrl).origin;
   const urls = pages.map((page) => {
-    const location = new URL(page.path, origin).toString();
+    const location = publicUrl(baseUrl, page.path);
     const lastmod = page.lastModified ? `\n    <lastmod>${xmlEscape(page.lastModified)}</lastmod>` : '';
     return `  <url>\n    <loc>${xmlEscape(location)}</loc>${lastmod}\n  </url>`;
   });
