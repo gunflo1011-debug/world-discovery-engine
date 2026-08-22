9bfe178e68981ca98aef2bd5015ae8d1efd66f8c
gunflo1011-debug <gunflo1011@gmail.com>
2026-08-23 00:02:09 +0200

    Add internet-use country profiles to discovery outputs

---
+++ scripts/finalize-internet-country-discovery.mjs
@@ -0,0 +1,43 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+const siteRoot = new URL('../site/', import.meta.url);
+const sitemapUrl = new URL('sitemap.xml', siteRoot);
+const buildUrl = new URL('build.json', siteRoot);
+const countryIndexUrl = new URL('indicators/internet-use/country/index.json', siteRoot);
+const baseUrl = process.env.SITE_BASE_URL || 'https://gunflo1011-debug.github.io/world-discovery-engine/';
+
+const [sitemap, rawBuild, rawIndex] = await Promise.all([
+  readFile(sitemapUrl, 'utf8'),
+  readFile(buildUrl, 'utf8'),
+  readFile(countryIndexUrl, 'utf8')
+]);
+
+const build = JSON.parse(rawBuild);
+const index = JSON.parse(rawIndex);
+if (index?.indicator !== 'IT.NET.USER.ZS' || !Number.isInteger(index?.observationYear) || !Array.isArray(index?.countries)) {
+  throw new Error('invalid internet-use country discovery index');
+}
+
+const existing = new Set([...sitemap.matchAll(/<loc>([^<]+)<\/loc>/g)].map((match) => match[1]));
+const additions = index.countries
+  .map((country) => new URL(country.url.replace(/^\//, ''), baseUrl).href)
+  .filter((url) => !existing.has(url));
+
+let updatedSitemap = sitemap;
+if (additions.length) {
+  const xml = additions.map((url) => `  <url><loc>${url.replaceAll('&', '&amp;')}</loc></url>`).join('\n');
+  updatedSitemap = sitemap.replace(/\s*<\/urlset>\s*$/, `\n${xml}\n</urlset>\n`);
+}
+
+const updatedBuild = {
+  ...build,
+  publicRoutes: Number(build.publicRoutes || 0) + additions.length,
+  internetUseCountryProfiles: index.countries.length
+};
+
+await Promise.all([
+  writeFile(sitemapUrl, updatedSitemap, 'utf8'),
+  writeFile(buildUrl, `${JSON.stringify(updatedBuild, null, 2)}\n`, 'utf8')
+]);
+
+console.log(`Added ${additions.length} internet-use country routes to discovery outputs.`);
