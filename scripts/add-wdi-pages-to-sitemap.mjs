f423dbf46cd3480ddfda1ee91b4db41b54c38e4e
gunflo1011-debug <gunflo1011@gmail.com>
2026-08-31 00:01:29 +0200

    Add country hubs to sitemap

--- scripts/add-wdi-pages-to-sitemap.mjs
+++ scripts/add-wdi-pages-to-sitemap.mjs
@@ -2,10 +2,16 @@ import { readFile, writeFile } from 'node:fs/promises';
 
 const siteRoot = new URL('../site/', import.meta.url);
 const catalog = JSON.parse(await readFile(new URL('data/wdi/index.json', siteRoot), 'utf8'));
+const countries = JSON.parse(await readFile(new URL('countries/index.json', siteRoot), 'utf8'));
 const sitemapUrl = new URL('sitemap.xml', siteRoot);
 let sitemap = await readFile(sitemapUrl, 'utf8');
 const base = 'https://worlddiscoverydata.com';
-const urls = ['/data/', ...(catalog.indicators ?? []).filter((x) => x.status === 'CURRENT_VERIFIED').map((x) => `/data/${x.slug}/`)];
+const urls = [
+  '/data/',
+  ...(catalog.indicators ?? []).filter((x) => x.status === 'CURRENT_VERIFIED').map((x) => `/data/${x.slug}/`),
+  '/countries/',
+  ...(countries.countries ?? []).map((x) => x.url)
+];
 
 for (const path of urls) {
   const row = `<url><loc>${base}${path}</loc></url>`;
@@ -13,4 +19,4 @@ for (const path of urls) {
 }
 
 await writeFile(sitemapUrl, sitemap, 'utf8');
-console.log(`Ensured ${urls.length} WDI catalog routes are present in sitemap.xml.`);
+console.log(`Ensured ${urls.length} WDI discovery routes are present in sitemap.xml.`);
