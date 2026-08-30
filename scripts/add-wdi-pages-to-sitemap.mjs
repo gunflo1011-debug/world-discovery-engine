e809d85788913a8935576df4e1840e58b7260162
gunflo1011-debug <gunflo1011@gmail.com>
2026-08-30 20:01:26 +0200

    Add WDI indicator pages to sitemap

---
+++ scripts/add-wdi-pages-to-sitemap.mjs
@@ -0,0 +1,16 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+const siteRoot = new URL('../site/', import.meta.url);
+const catalog = JSON.parse(await readFile(new URL('data/wdi/index.json', siteRoot), 'utf8'));
+const sitemapUrl = new URL('sitemap.xml', siteRoot);
+let sitemap = await readFile(sitemapUrl, 'utf8');
+const base = 'https://worlddiscoverydata.com';
+const urls = ['/data/', ...(catalog.indicators ?? []).filter((x) => x.status === 'CURRENT_VERIFIED').map((x) => `/data/${x.slug}/`)];
+
+for (const path of urls) {
+  const row = `<url><loc>${base}${path}</loc></url>`;
+  if (!sitemap.includes(row)) sitemap = sitemap.replace('</urlset>', `${row}\n</urlset>`);
+}
+
+await writeFile(sitemapUrl, sitemap, 'utf8');
+console.log(`Ensured ${urls.length} WDI catalog routes are present in sitemap.xml.`);
