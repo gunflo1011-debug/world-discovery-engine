a3af5c2c85a6fef0c61ea32622f3a9afc3898cf6
gunflo1011-debug <gunflo1011@gmail.com>
2026-09-04 17:22:33 +0200

    Encode country and territory heading safely

--- scripts/clarify-country-territory-directory.mjs
+++ scripts/clarify-country-territory-directory.mjs
@@ -4,7 +4,7 @@ const directoryUrl = new URL('../site/countries/index.html', import.meta.url);
 let html = await readFile(directoryUrl, 'utf8');
 
 html = html
-  .replaceAll('COUNTRY PROFILES', 'COUNTRY & TERRITORY PROFILES')
+  .replaceAll('COUNTRY PROFILES', 'COUNTRY &amp; TERRITORY PROFILES')
   .replaceAll('country profiles.', 'country and territory profiles.')
   .replaceAll('country profiles built from', 'country and territory profiles built from')
   .replaceAll('No country profiles match this search.', 'No country or territory profiles match this search.');
