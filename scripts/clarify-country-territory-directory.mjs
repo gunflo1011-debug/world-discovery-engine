aadbd90c5a20ff41fcdd5f31df5d48fd8729a963
gunflo1011-debug <gunflo1011@gmail.com>
2026-09-04 13:21:15 +0200

    Make country territory wording postprocess robust

--- scripts/clarify-country-territory-directory.mjs
+++ scripts/clarify-country-territory-directory.mjs
@@ -3,16 +3,12 @@ import { readFile, writeFile } from 'node:fs/promises';
 const directoryUrl = new URL('../site/countries/index.html', import.meta.url);
 let html = await readFile(directoryUrl, 'utf8');
 
-const replacements = [
-  [' COUNTRY PROFILES · OFFICIAL WORLD BANK DATA', ' COUNTRY & TERRITORY PROFILES · OFFICIAL WORLD BANK DATA'],
-  ['Showing all '+"' + cards.length + '"+' country profiles.', 'Showing all '+"' + cards.length + '"+' country and territory profiles.'],
-  ['Showing '+"' + visible + ' of ' + cards.length + '"+' country profiles.', 'Showing '+"' + visible + ' of ' + cards.length + '"+' country and territory profiles.'],
-  ['No country profiles match this search.', 'No country or territory profiles match this search.'],
-  ['Official WDI country profiles.', 'Official WDI country and territory profiles.'],
-  ['Browse country profiles built from 30 official World Bank WDI indicators', 'Browse country and territory profiles built from 30 official World Bank WDI indicators']
-];
-
-for (const [from, to] of replacements) html = html.replaceAll(from, to);
+html = html
+  .replaceAll('COUNTRY PROFILES', 'COUNTRY & TERRITORY PROFILES')
+  .replaceAll('country profiles.', 'country and territory profiles.')
+  .replaceAll('country profiles built from', 'country and territory profiles built from')
+  .replaceAll('No country and territory profiles match this search.', 'No country or territory profiles match this search.')
+  .replaceAll('Official WDI country and territory profiles.', 'Official WDI country and territory profiles.');
 
 await writeFile(directoryUrl, html, 'utf8');
 console.log('Clarified country and territory directory wording.');
