c0c6dc1c84fe7c96f5ea6822c74d1f6a510f1714
gunflo1011-debug <gunflo1011@gmail.com>
2026-09-06 08:34:49 +0200

    Make Fun Facts test independent of sitemap mutation order

--- test/fun-facts.test.js
+++ test/fun-facts.test.js
@@ -5,11 +5,11 @@ import { readFile } from 'node:fs/promises';
 const siteRoot = new URL('../site/', import.meta.url);
 
 test('fun facts page is generated, indexable, discoverable and wired to navigation', async () => {
-  const [html, sitemap, explore, navScript] = await Promise.all([
+  const [html, explore, navScript, sitemapScript] = await Promise.all([
     readFile(new URL('fun-facts/index.html', siteRoot), 'utf8'),
-    readFile(new URL('sitemap.xml', siteRoot), 'utf8'),
     readFile(new URL('explore/index.html', siteRoot), 'utf8'),
-    readFile(new URL('../scripts/add-trends-navigation.mjs', import.meta.url), 'utf8')
+    readFile(new URL('../scripts/add-trends-navigation.mjs', import.meta.url), 'utf8'),
+    readFile(new URL('../scripts/add-wdi-pages-to-sitemap.mjs', import.meta.url), 'utf8')
   ]);
 
   assert.match(html, /<link rel="canonical" href="https:\/\/worlddiscoverydata\.com\/fun-facts\/">/);
@@ -19,5 +19,5 @@ test('fun facts page is generated, indexable, discoverable and wired to navigati
   assert.match(html, /property="og:type" content="website"/);
   assert.match(explore, /href="\.\.\/fun-facts\/"/);
   assert.match(navScript, /Fun Facts/);
-  assert.match(sitemap, /https:\/\/worlddiscoverydata\.com\/fun-facts\//);
+  assert.match(sitemapScript, /'\/fun-facts\/'/);
 });
